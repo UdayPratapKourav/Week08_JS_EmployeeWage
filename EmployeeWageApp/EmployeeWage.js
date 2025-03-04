a5171972796af0d56484e21a917719ffdc66bea4
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-03-04 22:42:26 +0530

    [Uday] Update. Refactored code to use function

--- EmployeeWageApp/EmployeeWage.js
+++ EmployeeWageApp/EmployeeWage.js
@@ -11,18 +11,22 @@ const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
-const WEGE_PER_HOUR=20;
+const WAGE_PER_HOUR=20;
+
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
 
 let empHrs=0;
 empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-    default:
-        empHrs=0;
-}
-let empWage=empHrs*WEGE_PER_HOUR;
+empHrs=getWorkingHours(empCheck);
+let empWage=empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage : "+empWage);
