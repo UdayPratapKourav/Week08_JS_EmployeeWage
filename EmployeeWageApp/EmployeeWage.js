5a3453207983d0addac4ffdb41b1d63aa406dbd3
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-03-04 23:09:54 +0530

    [Uday] add, Calculating Wages till Number of Working Days or Total Working Hours per month is Reached

--- EmployeeWageApp/EmployeeWage.js
+++ EmployeeWageApp/EmployeeWage.js
@@ -12,6 +12,11 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const MAX_HRS_IN_MONTH=100;
+const NUM_OF_WORKING_DAYS=10;
+let totalEmpHrs=0;
+let totalWoringDays=0;
+
 
 function getWorkingHours(empCheck){
     switch(empCheck){
@@ -31,10 +36,18 @@ let empHrs=0;
 // // let empWage=empHrs*WAGE_PER_HOUR;
 // console.log("Emp Wage : "+empWage);
 
-const NUM_OF_WORKING_DAYS=2;
+
 for(let day =0;day<NUM_OF_WORKING_DAYS;day++){
     let empCheck=Math.floor(Math.random()*10)%3;
     empHrs+=getWorkingHours(empCheck);
 }  
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Total Hrs : "+empHrs+" Emp wage : "+empWage);
+// let empWage=empHrs*WAGE_PER_HOUR;
+// console.log("Total Hrs : "+empHrs+" Emp wage : "+empWage);
+
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWoringDays<NUM_OF_WORKING_DAYS){
+    totalWoringDays++;
+    let empCheck=Math.floor(Math.random()*10)%3;
+    totalEmpHrs+=getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+console.log("UC5-Total Days : "+totalWoringDays+" Total Hrs : "+totalEmpHrs+"Emp wage : "+empWage);
