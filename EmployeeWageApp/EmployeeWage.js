90bc83da35aeeef23a8129f7745d9d2c862886a0
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-03-04 22:58:40 +0530

    [Uday] Add. calculating wages for a month

--- EmployeeWageApp/EmployeeWage.js
+++ EmployeeWageApp/EmployeeWage.js
@@ -26,7 +26,15 @@ function getWorkingHours(empCheck){
 }
 
 let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-empHrs=getWorkingHours(empCheck);
+// empCheck=Math.floor(Math.random()*10)%3;
+// empHrs=getWorkingHours(empCheck);
+// // let empWage=empHrs*WAGE_PER_HOUR;
+// console.log("Emp Wage : "+empWage);
+
+const NUM_OF_WORKING_DAYS=2;
+for(let day =0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck);
+}  
 let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Emp Wage : "+empWage);
+console.log("Total Hrs : "+empHrs+" Emp wage : "+empWage);
